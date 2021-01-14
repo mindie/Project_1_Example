c18bc11d92b5d4e03d595dc71dd2b38f535225cc
Mindie Brockwell <mindiepaige@gmail.com>
2021-01-14 17:34:56 -0500

    Initial commit

---
+++ first_file.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+from boto3_type_annotations.sqs import ServiceResource, Queue, Message
+
+DATA_QUEUE_REGION = 'us-west-1'
+
+sqs = boto3.resource('sqs',DATA_QUEUE_REGION)
+queue = sqs.get_queue_by_name(QueueName='test_status_queue.fifo')
+status_message = {
+        'uuid': 7266,
+        'title': 'Needs Validation',
+        'description': 'file processed',
+        'status': 'needs_validation',
+    }
+response = queue.send_message(
+    MessageBody=json.dumps(status_message),
+    MessageGroupId='test_status'
+)
+
+print(response)
