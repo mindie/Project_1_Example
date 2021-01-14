1e676904aa5611f699ee6b9aabb87c331ea84291
Mindie Brockwell <mindiepaige@gmail.com>
2021-01-14 17:38:05 -0500

    Changing file

--- first_file.py
+++ first_file.py
@@ -1,20 +1,14 @@
 import boto3
-import json
-from boto3_type_annotations.sqs import ServiceResource, Queue, Message
 
-DATA_QUEUE_REGION = 'us-west-1'
+sqs = boto3.client('sqs')
 
-sqs = boto3.resource('sqs',DATA_QUEUE_REGION)
-queue = sqs.get_queue_by_name(QueueName='test_status_queue.fifo')
-status_message = {
-        'uuid': 7266,
-        'title': 'Needs Validation',
-        'description': 'file processed',
-        'status': 'needs_validation',
-    }
-response = queue.send_message(
-    MessageBody=json.dumps(status_message),
-    MessageGroupId='test_status'
+queue_url = 'https://sqs.us-east-1.amazonaws.com/618573814374/dev-serverless-api-google-ads-connector-queue'
+
+# Send message to SQS queue
+response = sqs.send_message(
+    QueueUrl=queue_url,
+    MessageBody=("{\"accounts\": [3036115467414, 341156790566843], \"brand_id\": 379, \"brand_ids\": [379], \"user_uploaded\": \"mindie.brockwell@keends.com\", \"connector_id\": 140,\"end_date\": \"2019-01-01\",\"file_name\": \"Facebook Lambda Test\",\"region\": \"us-east-1\",\"start_date\": \"2018-01-01\"}")
 )
+
 
 print(response)
